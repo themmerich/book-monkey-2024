34afedf2fa2154e65a54856a01c2c5a11e8e4878
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2024-04-15 20:52:37 +0200

    added component input binding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,7 +8,7 @@ import { authInterceptor } from './shared/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
   ],
--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { Book } from '../../shared/book';
 import { BookStoreService } from '../../shared/book-store.service';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,11 +12,11 @@ import { Observable } from 'rxjs';
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.css',
 })
-export class BookDetailsComponent {
-  book$: Observable<Book>;
+export class BookDetailsComponent implements OnInit {
+  book$: Observable<Book> | undefined;
   bookStoreService = inject(BookStoreService);
-  route = inject(ActivatedRoute);
   router = inject(Router);
+  @Input() isbn: string = '';
 
   constructor() {
     // TODO: subscribe on paramMap and then on service
@@ -27,8 +27,8 @@ export class BookDetailsComponent {
         }),
       )
       .subscribe();*/
-    const isbn = this.route.snapshot.paramMap.get('isbn')!;
-    this.book$ = this.bookStoreService.getSingle(isbn);
+    //const isbn = this.route.snapshot.paramMap.get('isbn')!;
+    //this.book$ = this.bookStoreService.getSingle(isbn);
   }
 
   removeBook(isbn: string) {
@@ -37,5 +37,9 @@ export class BookDetailsComponent {
         this.router.navigateByUrl('/books');
       });
     }
+  }
+
+  ngOnInit(): void {
+    this.book$ = this.bookStoreService.getSingle(this.isbn);
   }
 }
