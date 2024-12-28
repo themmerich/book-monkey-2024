f09f5671ae8acf419b6e4dd06bb47162e2cf677f
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2024-12-28 12:47:30 +0100

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-5XEJL7VV.mjs";import"./chunk-CTDURT6M.mjs";import"./chunk-KIUTNAEO.mjs";import"./chunk-MBBWET6Z.mjs";import"./chunk-LOAOCB3Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
